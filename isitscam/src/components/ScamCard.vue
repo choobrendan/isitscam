9896d4e13c11f8b6996e77beaa2f2314dbceb885
Looi Wei En <looi.weien02@student.usm.my>
2024-03-16 22:18:47 +0800

    add min width restriction

--- isitscam/src/components/ScamCard.vue
+++ isitscam/src/components/ScamCard.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div style="width: 100%;"><a href="#" class=" m-12 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<div style="width: 100%;"><a href="#" class=" m-12 block max-w-sm min-w-40 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{title}}</h5>
 <p class="font-normal text-gray-700 dark:text-gray-400">{{ description }}</p>
