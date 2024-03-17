dbb5d24a4542cd4f462be853a1a704501edcf38d
Looi Wei En <looi.weien02@student.usm.my>
2024-03-17 19:25:15 +0800

    fix structure

--- isitscam/tailwind.config.js
+++ isitscam/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [    "./index.html",
-  "./src/**/*.{vue,js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js"],
+  content: [    
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
