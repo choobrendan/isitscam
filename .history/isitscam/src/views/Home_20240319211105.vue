80e90d6c2bbbef9216b76442c295cc707b410da2
Brendan Choo <choobrendan@gmail.com>
2024-03-19 21:11:11 +0800

    Fixed button bug

--- .history/isitscam/src/views/Home_20240319211105.vue
+++ .history/isitscam/src/views/Home_20240319211105.vue
@@ -95,7 +95,7 @@
     </div>
     
     <div class="text-center">
-    <button type="button" @click="toggleSubmit" class="  m-4 w-4/6 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600   dark:focus:ring-gray-700">Submit</button>
+    <button type="button" class="  m-4 w-4/6 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600   dark:focus:ring-gray-700">Submit</button>
   </div>
   </div>
   
