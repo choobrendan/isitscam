df19826cf0ade329398571500b15e8b21bf59865
Looi Wei En <looi.weien02@student.usm.my>
2024-03-23 00:57:55 +0800

    modify navbar

--- isitscam/src/components/Navbar.vue
+++ isitscam/src/components/Navbar.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="fixed top-0 w-full bg-white h-[8vh] mb-2 px-4 flex items-center justify-between align-middle">
+  <!-- <div class="z-50 fixed top-0 w-full bg-white bg-opacity-85 h-[8vh] mb-2 px-4 flex items-center justify-between align-middle">
     <div class="flex items-center">
       <a href="/" class="mr-4">
         <logo :fontSize="24" />
       </a>
-      <!-- Add more links here -->
       <a href="/home" class="mr-4">Scam Detection</a>
       <a href="/report" class="mr-4">Discover</a>
     </div>
@@ -12,17 +11,47 @@
       <a href="/login" class="mr-4">Login</a>
       <a href="/register">Register</a>
     </div>
-  </div>
+  </div> -->
+  <fwb-navbar>
+    <template #logo>
+      <a href="/">
+        <logo :fontSize="24" />
+      </a>
+    </template>
+    <template #default="{ isShowMenu }">
+      <fwb-navbar-collapse :is-show-menu="isShowMenu">
+        <fwb-navbar-link link="report"> What is Scam? </fwb-navbar-link>
+        <fwb-navbar-link link="/home"> Scam Detection </fwb-navbar-link>
+        <fwb-navbar-link link="report"> Discover </fwb-navbar-link>
+      </fwb-navbar-collapse>
+    </template>
+    <template #right-side>
+      <fwb-navbar-collapse :is-show-menu="isShowMenu">
+        <fwb-navbar-link link="report"> Log In </fwb-navbar-link>
+      </fwb-navbar-collapse>
+      <fwb-button size="xs"> Sign Up </fwb-button>
+    </template>
+  </fwb-navbar>
 </template>
 
 <script>
-import Logo from './Logo.vue';
+import Logo from "./Logo.vue";
 
 export default {
   components: {
-    Logo
-  }
-}
+    Logo,
+  },
+};
+</script>
+
+<script setup>
+import {
+  FwbButton,
+  FwbNavbar,
+  FwbNavbarCollapse,
+  FwbNavbarLink,
+  FwbNavbarLogo,
+} from "flowbite-vue";
 </script>
 
 <style scoped>
