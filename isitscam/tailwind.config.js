7668483cc24095a32441b1f8b85ef54aca358b36
Looi Wei En <looi.weien02@student.usm.my>
2024-03-23 00:57:26 +0800

    change export default to module.exports

--- isitscam/tailwind.config.js
+++ isitscam/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [    
+module.exports = {
+  content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"],
+    "./node_modules/flowbite/**/*.js",
+    "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite')],
-}
+  plugins: [require("flowbite"), require("flowbite/plugin")],
+};
