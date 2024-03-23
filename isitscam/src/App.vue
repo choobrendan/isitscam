d7aa5239af14c9bee701f7346dce112ea96c901a
Looi Wei En <looi.weien02@student.usm.my>
2024-03-24 00:52:49 +0800

    change navbar position

--- isitscam/src/App.vue
+++ isitscam/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="fixed top-0 w-full z-50">
-    <navbar />
-  </div>
   <div class="flex align-middle justify-center m-16">
+    <div class="fixed top-0 w-full z-50">
+      <navbar />
+    </div>
     <router-view></router-view>
   </div>
 </template>
